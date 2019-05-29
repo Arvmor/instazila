b19ab4e93752fd64d8ea0e12891db82d02f0d2bd
Goodzilam <arvin.morawej@gmail.com>
2019-05-29 13:32:57 +0430

    Update login.py

--- login.py
+++ login.py
@@ -1,15 +1,27 @@
+#author : Goodzilam
+#tested on Python 3.7.3 and windows 10
+#how to use :
+#open cmd > type : pip install selenium
+#go to "http://chromedriver.chromium.org/downloads" and download the ChromeDriver which matches with your ***version***
+#***for finding your chrome version you have to check it at "chrome://settings/help" in your chrome browser***
+#then replace the downloaded ChromeDriver path to the marked line in the code
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 
 #logging in
-n = input('how many users: ')
+n = input('How many users do you want to gather ? ')
 u = input("username: ")
 p = input("password: ")
-t = input("Who is your target: ")
+t = input("Who is your target(id instagram): ")
 f= open("targets.txt","w")
-chromedriver = "C:/Users/123/Downloads/chromedriver.exe"
+
+#**************************************************************************
+chromedriver = "C:/Users/123/Downloads/chromedriver.exe" #replace the path here 
+#**************************************************************************
+
 driver = webdriver.Chrome(chromedriver)
 url = "https://instagram.com/"+t+"/followers"
 driver.get(url)
@@ -19,7 +31,6 @@ driver.find_element_by_name("password").send_keys(p)
 time.sleep(2)
 driver.find_element(By.XPATH, "//*[@id='react-root']/section/main/div/article/div/div[1]/div/form/div[4]/button").click()
 time.sleep(10)
-
 #Gathering steps
 x = 1
 y = int(n)+1
@@ -37,5 +48,5 @@ while x != y:
     f.write(res[26:-1]+"\n")
     print(res[26:-1]+" "+str(x)+"\n")
     x = x + 1
-print("done ...")
+print("done !!!")
 f.close()
